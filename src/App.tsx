2d5d95b5ab19807ff0fdb1dec0b930f4baa469b6
Slavashevich Oleg <oslavashevish@gmail.com>
2021-03-13 12:52:03 +0300

    Added Model component, to load models

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import { OrbitControls } from "@react-three/drei";
 import './App.css';
 import Model from './components/Model';
     
 function App() {
     return (
         <div className="App">
-            <Model/>
+            <Canvas camera={{ position: [0, 100, 100] }}>
+                <Suspense fallback={null}>
+                    <Model />
+                </Suspense>
+                <OrbitControls />
+            </Canvas>
         </div>
     );
 }
--- src/components/Model/index.tsx
+++ src/components/Model/index.tsx
@@ -1,11 +1,36 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useRef, useEffect } from 'react';
+import * as THREE from "three";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
+import { useLoader, useThree } from "react-three-fiber";
+import { useHelper } from "@react-three/drei";
+import './style.scss';
 
 interface IProps {
 
 }
 
 const Model: FunctionComponent<IProps> = (props: IProps) => {
-    return <div>Model</div>;
+    const geom = useLoader(STLLoader, "book.stl");
+
+    const ref = useRef<any>(null);
+    const { camera } = useThree();
+
+    useEffect(() => {
+        if (ref.current) camera.lookAt(ref.current.position);
+    });
+
+    useHelper(ref, THREE.BoxHelper, "#000");
+
+    return (
+        <>
+            <mesh ref={ref} onClick={(result) => console.log(result)}>
+                <primitive object={geom} attach="geometry" />
+                <meshStandardMaterial color={"orange"} />
+            </mesh>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+        </>
+    );
 }
 
 export default Model;
