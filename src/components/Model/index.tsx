b376beb39617897fa21d1d4945395068ef941f0d
Oleg Slavashevich <oslavashevish@gmail.com>
2021-03-13 11:49:52 +0300

    Change spaces in app

---
+++ src/components/Model/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+interface IProps {
+
+}
+
+const Model: FunctionComponent<IProps> = (props: IProps) => {
+    return <div>Model</div>;
+}
+
+export default Model;
