cedc9f2bed1908258198c2abd4f189964ca7c905
Slavashevich Oleg <oslavashevish@gmail.com>
2021-03-23 21:16:28 +0300

    Added sphere to picked point
    :wq

--- src/components/Model/index.tsx
+++ src/components/Model/index.tsx
@@ -1,33 +1,41 @@
-import React, { FunctionComponent, useRef, useEffect } from 'react';
+import React, { FunctionComponent, useRef, useEffect, useState } from 'react';
 import * as THREE from "three";
 import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
 import { useLoader, useThree } from "react-three-fiber";
 import './style.scss';
-import { useHelper } from "@react-three/drei";
+import { Vector3 } from 'three';
 
 interface IProps {
 
 }
 
 const Model: FunctionComponent<IProps> = (props: IProps) => {
-    const { scene } = useThree();
+    const { camera, scene } = useThree();
     const axesHelper = new THREE.AxesHelper( 1000 );
     scene.add(axesHelper);
-    const geom = useLoader(STLLoader, "testcube.stl").center();
+    const geom: THREE.BufferGeometry = useLoader(STLLoader, "testcube.stl").center();
+    geom.name = 'MyCube_s';
     const ref = useRef<any>(null);
-    const { camera } = useThree();
+
+    const [pointCoords, setPointCoords] = useState<Vector3 | undefined>(undefined); 
 
     useEffect(() => {
         if (ref.current) camera.lookAt(ref.current.position);
     });
 
-    useHelper(ref, THREE.BoxHelper, "#000");
+    function handleGeometryClick(event: any): void {
+        setPointCoords(event.point);
+    }
 
     return (
         <>
-            <mesh ref={ref} onClick={(result) => console.log(result)}>
+            <mesh ref={ref} onClick={handleGeometryClick}>
                 <primitive object={geom} attach="geometry" />
-                <meshStandardMaterial color={"orange"} />
+                <meshStandardMaterial color={"#A9A9A9"} />
+            </mesh>
+            <mesh position={pointCoords} ref={ref} onClick={handleGeometryClick}>
+                <sphereGeometry attach="geometry" args={[0.4, 16, 16]} />
+                <meshStandardMaterial color={"red"} />
             </mesh>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
